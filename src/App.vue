7fd163956991caf70bf286d18d4356ae51295557
shuantt <tehsuan.tht@gmail.com>
2024-07-04 00:04:03 +0800

    feat: 新增 header 漢堡選單樣式(未完成)

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,42 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavItem from './components/NavItem.vue'
+import MenuIcon from 'vue-material-design-icons/Menu.vue'
+import logo from '@/assets/logo-noel.svg'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <header
+    class="relative mx-auto flex items-center justify-between border-b-[1px] border-solid border-[#F1F1F1] p-3"
+  >
+    <h1>
+      <img :src="logo" alt="logo" />
+    </h1>
+    <div>
+      <MenuIcon fillColor="black" :size="30" class="text-center" />
     </div>
+    <nav
+      class="absolute bottom-0 left-0 right-0 top-full mx-auto flex w-full flex-col p-7 text-center"
+    >
+      <RouterLink class="group" to="/">
+        <NavItem item-text="首頁" />
+      </RouterLink>
+      <RouterLink class="group" to="/">
+        <NavItem item-text="作品集" />
+      </RouterLink>
+      <RouterLink class="group" to="/">
+        <NavItem item-text="服務項目" />
+      </RouterLink>
+      <RouterLink class="group" to="/">
+        <NavItem item-text="部落格" />
+      </RouterLink>
+      <RouterLink class="group" to="/">
+        <NavItem item-text="聯絡我" />
+      </RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps(['itemText'])
+console.log(props.itemText)
+</script>
+<template>
+  <span class="relative px-2 py-1 text-xl">
+    <span class="group-hover:font-bold">{{ itemText }}</span>
+    <div
+      class="absolute left-0 top-0 hidden h-2 w-2 border-l-[1px] border-t-[1px] border-solid border-[#919191] group-hover:block"
+    ></div>
+    <div
+      class="absolute right-0 top-0 hidden h-2 w-2 border-r-[1px] border-t-[1px] border-solid border-[#919191] group-hover:block"
+    ></div>
+    <div
+      class="absolute bottom-0 left-0 hidden h-2 w-2 border-b-[1px] border-l-[1px] border-solid border-[#919191] group-hover:block"
+    ></div>
+    <div
+      class="absolute bottom-0 right-0 hidden h-2 w-2 border-b-[1px] border-r-[1px] border-solid border-[#919191] group-hover:block"
+    ></div>
+  </span>
+</template>
